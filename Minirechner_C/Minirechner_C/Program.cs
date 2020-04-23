7c4c0467cd490c24830b7184772701a877650adf
02mazjul@rgtfo-me.it <62942784+JulianMazohl@users.noreply.github.com>
2020-04-23 14:36:29 +0200

    Commit Subtraktion

--- Minirechner_C/Minirechner_C/Program.cs
+++ Minirechner_C/Minirechner_C/Program.cs
@@ -13,22 +13,39 @@ namespace Minirechner_C
             int Zahl1 = 0;
             int Zahl2 = 0;
             int Ergebnis = 0;
+            string Operator;
 
             Console.WriteLine("Gib deine 1. Zahl ein: ");
             Zahl1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Gib deine 2. Zahl ein: ");
             Zahl2 = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("Gib die Operation ein (Addition, Subtraktion)....achte auf die Rechtsschreibung!!!");
+            Operator = Console.ReadLine();
+
+            if (Operator == "Addition")
+            {
             Ergebnis = Addition(Zahl1, Zahl2);
+            }
+
+            if (Operator == "Subtraktion")
+            {
+                Ergebnis = Subtraktion(Zahl1, Zahl2);
+            }
+
             Console.WriteLine("Das Ergebnis ist: " + Ergebnis);
             Console.ReadKey();
         }
 
         static int Addition(int zahl1, int zahl2)
         {
-            int erg = zahl1 + zahl2;
-            return erg;
+            int ergebnis = zahl1 + zahl2;
+            return ergebnis;
         }
-       
+        static int Subtraktion(int zahl1,int zahl2)
+        {
+            int ergebnis = zahl1 - zahl2;
+            return ergebnis;
+        }
     }
 }
